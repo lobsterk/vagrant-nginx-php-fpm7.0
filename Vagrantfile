f5ede1ee017cb9f13bb6170e435a24159eadb455
Danil Kopylov <lobsterk@yandex.ru>
2017-02-06 00:46:56 +0300

    add 2 file: set param in .const file, add bootstrap.sh with rules install mysql, php7 etc. share folder www and nginx

--- Vagrantfile
+++ Vagrantfile
@@ -7,29 +7,29 @@ Vagrant.configure(2) do |config|
     config.vm.box = "ubuntu/trusty64"
 
     config.vm.network "private_network", ip: "192.168.10.10"
-    config.vm.hostname = "vm-new"
+    config.vm.hostname = "wm-php7"
 
     config.vm.post_up_message = "Congratulations. You ok !!! \n http:\\192.168.10.10"
 
-#    config.vm.provision :shell, :path => "bootstrap.sh"
+    config.vm.provision :shell, :path => "bootstrap.sh"
 
     config.vm.provider "virtualbox" do |v|
         v.memory = 1024
         v.cpus = 1
     end
 
-#    config.vm.provider "virtualbox" do |v, override|
-#
-#        override.vm.synced_folder "./www", "/var/www",
-#            id: "www-data",
-#            owner: "www-data", group: "www-data",
-#            mount_options: ["dmode=775,fmode=664"]
-#
-#        override.vm.synced_folder "./nginx/sites-enabled", "/etc/nginx/sites-enabled",
-#            id: "nginx",
-#            owner: "root", group: "root",
-#            mount_options: ["dmode=775,fmode=664"]
-#
-#    end
+    config.vm.provider "virtualbox" do |v, override|
+
+        override.vm.synced_folder "./www", "/var/www",
+            id: "www-data",
+            owner: "www-data", group: "www-data",
+            mount_options: ["dmode=775,fmode=664"]
+
+        override.vm.synced_folder "./nginx/sites-enabled", "/etc/nginx/sites-enabled",
+            id: "nginx",
+            owner: "root", group: "root",
+            mount_options: ["dmode=775,fmode=664"]
+
+    end
 
 end
---
+++ bootstrap.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+
+### Set script params
+
+source /vagrant/.const
+
+#MySQL
+function install_mysql_server()
+{
+    debconf-set-selections <<< "mysql-server mysql-server/root_password password ${DB_PASS}"
+    debconf-set-selections <<< "mysql-server mysql-server/root_password_again password ${DB_PASS}"
+
+    apt-get -qq update
+    apt-get install -y mysql-server
+
+    mysql -u${DB_USER} -p${DB_PASS} -e "CREATE DATABASE ${DB_NAME}"
+}
+
+#WebServer
+function install_webserver()
+{
+    add-apt-repository ppa:ondrej/php -y
+    apt-get -qq update
+    apt-get install -y mc curl \
+                       nginx \
+                       sshfs \
+                       php7.0-fpm \
+                       php7.0-common \
+                       php7.0-cli \
+                       php7.0-gd \
+                       php7.0-mysql \
+                       php7.0-curl \
+                       php-pear \
+                       php7.0-dev \
+                       php7.0-mongo \
+                       make \
+                       openssl \
+                       libssl-dev \
+                       libcurl4-openssl-dev \
+                       libsasl2-dev \
+                       libpcre3-dev \
+		               php7.0-mcrypt
+
+    adduser vagrant www-data
+}
+
+#additional
+function install_additional()
+{
+    apt-get -qq update
+    apt-get install -y libjpeg-progs \
+                       optipng \
+                       gifsicle \
+                       imagemagick
+}
+
+install_mysql_server
+install_webserver
+install_additional
+
